f079f64661eec8a383259fa09d9dd039aad4815d
enderdude77 <130871478+enderdude77@users.noreply.github.com>
2023-06-03 10:31:23 -0700

    Delete Startup.sh

--- Startup.sh
+++
@@ -1,5 +0,0 @@
- #!/bin/bash
- #fedora startup script
- sudo dnf install discord steam @xfce-desktop-environment firefox sddm gnome-software -y
- sudo systemctl enable sddm
- sudo systemctl set-default graphical.target
